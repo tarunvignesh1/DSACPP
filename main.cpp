fa356eb3c8cb18a467fe8ce5615bca948b7dcccf
Tarun Vignesh Selvabalaji <tarunvignesh1@gmail.com>
2022-04-07 21:42:34 +0530

    just refreshing CPP

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+/* Create a program that takes number as input and returns its square root in the form of a string.*/
+
+
+#include <iostream>
+#include <cmath>
+
+
+
+
+int main()
+{
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    std::cout << "The square root of " << number << " is " << sqrt(number) << std::endl;
+    return 0;
 }
